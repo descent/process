3123664d6fdb7ef52001e39d146626803dc1a135
descent <descent@weiya.com.tw>
2013-11-25 13:17:45 +0800

    add MDIR for mount dir

--- cp.sh
+++ cp.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-sudo mount -o loop dos622.img  /mnt/
-sudo cp -v process.com  /mnt/
-sudo cp -v simple_proc.com  /mnt/sp.com
-sudo umount /mnt/
+MDIR=/mnt
+sudo mount -o loop dos622.img  $MDIR
+sudo cp -v process.com  $MDIR
+sudo cp -v simple_proc.com  $MDIR
+sudo umount $MDIR
 
