66ea84e2807984313335f5c5b88dbbc0aee92118
descent <descent0@gmail.com>
2020-02-13 08:56:33 +0800

    cp simple_proc_signal_putch.com

--- cp.sh
+++ cp.sh
@@ -4,4 +4,5 @@ sudo mount -o loop dos622.img  $MDIR
 sudo cp -v process.com  $MDIR
 sudo cp -v simple_proc.com  $MDIR
 sudo cp -v simple_proc_signal.com  /mnt/sps.com
+sudo cp -v simple_proc_signal_putch.com  /mnt/spsch.com
 sudo umount $MDIR
